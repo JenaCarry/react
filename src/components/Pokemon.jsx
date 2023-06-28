c1191c854c5901262ea954bcf4204c05d484a18a
Jean <120676537+JenaCarry@users.noreply.github.com>
2023-06-28 13:44:16 -0300

    feat: add props to Pokemon

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,3 +1,5 @@
-export const Pokemon = () => {
-    return <h2>Nome do pokemon: Pikachu</h2>
-}
+export const Pokemon = ({ name = "nome não encontrado!" }) => {
+  //   return <h2>Nome do pokemon: {name || "nome não encontrado!"}</h2>;
+  //   return <h2>Nome do pokemon: {name ? name : "nome não encontrado!"}</h2>;
+  return <h2>Nome do pokemon: {name}</h2>;
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,10 @@ export const Home = () => {
   return (
     <div>
       <h1>Meu site de Pokemon</h1>
-      <Pokemon />
+      <Pokemon name="Pikachu" />
+      <Pokemon name="Charmander" />
+      <Pokemon name="Squirtle" />
+      <Pokemon name="Ditto" />
     </div>
   );
 };
