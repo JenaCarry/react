f3cfb829af57f2f1444c191b4dbc719015908efd
Jean <120676537+JenaCarry@users.noreply.github.com>
2023-06-28 14:20:01 -0300

    feat: new folder structure

--- src/components/Pokemon.jsx
+++
@@ -1,7 +0,0 @@
-import "./style.css";
-
-export const Pokemon = ({ name = "nome não encontrado!" }) => (
-  //   return <h2>Nome do pokemon: {name || "nome não encontrado!"}</h2>;
-  //   return <h2>Nome do pokemon: {name ? name : "nome não encontrado!"}</h2>;
-  <h2 className="title">Nome do pokemon: {name}</h2>
-);
---
+++ src/components/Pokemon/index.jsx
@@ -0,0 +1,3 @@
+export const Pokemon = ({ name = "nome não encontrado!" }) => (
+  <h2>Nome do pokemon: {name}</h2>
+);
